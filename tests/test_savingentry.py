62710d96508737531c2f412f48214b65ca049c94
gopika0003 <gopikasuresh1210@gmail.com>
2025-04-19 15:41:55 +0530

    Added test

--- tests/test_savingentry.py
+++ tests/test_savingentry.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager  # For automatic ChromeDriver setup
-import os  # For handling environment variables
+import os 
 
 class TestSavingEntry:
     def setup_method(self, method):
